8f7edd58cdf65fc8d362013931f320a82712ce2e
David Oliveira <david@Davids-MacBook-Air.local>
2021-07-11 14:09:31 +0200

    A couple of fixes and protection against the race condition of running two logins at the same time

--- test/huawei-cli.js
+++ test/huawei-cli.js
@@ -4,7 +4,7 @@ const { HuaweiRouter } = require('..');
 const router = new HuaweiRouter({
   username: process.env.HUAWEI_USERNAME || 'admin',
   password: process.env.HUAWEI_PASSWORD || 'admin',
-//  ip: '10.2.2.18',
+  ip: '10.2.2.18',
   headers: {'Host': '192.168.1.1'},
 });
 
