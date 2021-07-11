5c9665088e2b6766ef3b407b1d56b743c8eb3fd4
David Oliveira <david@Davids-MacBook-Air.local>
2021-07-11 10:06:52 +0200

    Moved getDeviceInfo to auth'ed. Added debugging support and removed noise

--- test/huawei-cli.js
+++ test/huawei-cli.js
@@ -4,10 +4,15 @@ const { HuaweiRouter } = require('..');
 const router = new HuaweiRouter({
   username: process.env.HUAWEI_USERNAME || 'admin',
   password: process.env.HUAWEI_PASSWORD || 'admin',
-  ip: '10.2.2.18',
+//  ip: '10.2.2.18',
   headers: {'Host': '192.168.1.1'},
 });
 
 (async () => {
   console.log("Router info:", await router.getSystemInfo());
+  console.log("Mobile network info: ", await router.getMobileNetworkInfo());
+  console.log("Basic info: ", await router.getBasicInfo());
+  console.log("Device info: ", await router.getDeviceInfo());
+  console.log("System info: ", await router.getSystemInfo());
+  console.log("Status: ", await router.getStatus());
 })();
