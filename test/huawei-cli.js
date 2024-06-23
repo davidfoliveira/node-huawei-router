41674eac08001052751437ceb5d86ae20e8c4c73
David Oliveira <d.oliveira@prozone.org>
2024-06-23 21:13:54 +0100

    Update from local

--- test/huawei-cli.js
+++ test/huawei-cli.js
@@ -4,15 +4,22 @@ const { HuaweiRouter } = require('..');
 const router = new HuaweiRouter({
   username: process.env.HUAWEI_USERNAME || 'admin',
   password: process.env.HUAWEI_PASSWORD || 'admin',
-  ip: '10.2.2.18',
-  headers: {'Host': '192.168.1.1'},
+  ip: process.env.HUAWEI_HOST || '192.168.0.1',
+  headers: {'Host': process.env.HUAWEI_HOST || '192.168.0.1'},
+  debug: true,
 });
 
 (async () => {
+
   console.log("Router info:", await router.getSystemInfo());
   console.log("Mobile network info: ", await router.getMobileNetworkInfo());
   console.log("Basic info: ", await router.getBasicInfo());
   console.log("Device info: ", await router.getDeviceInfo());
   console.log("System info: ", await router.getSystemInfo());
   console.log("Status: ", await router.getStatus());
+
+  console.log("Messages: ", await router.listSMS(null, 1, 10));
+
+  console.log("Logout: ", await router.logout());
+
 })();
